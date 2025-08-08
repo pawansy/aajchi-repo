6f25ff57286f85ace8a332b881f0db40e939f5f2
Pawan <pawanyadav@gmail.com>
2025-08-08 12:23:10 +0000

    commit5

--- aajchifile.java
+++ aajchifile.java
@@ -1,5 +1,5 @@
 public class aajchifile {
     public static void main(String[] args) {
-        System.out.println("Hello maza, kingwwworld!");
+        System.out.println("Hello maza, kingworld!");
     }
 }
